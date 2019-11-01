8f8868a047610af26b565b58f42439a3e9069fbd
furico <furihatah@gmail.com>
2019-11-01 22:41:51 +0900

    Add functions

--- src/tutorial/scrape1.clj
+++ src/tutorial/scrape1.clj
@@ -7,4 +7,11 @@
   (html/html-resource (java.net.URL. url)))
 
 (defn hn-headlines []
-  (map html/text (html/select (fetch-url *base-url*) [:td.title :a])))
+  (map html/text (html/select (fetch-url *base-url*) [:td.title :> :a])))
+
+(defn hn-points []
+  (map html/text (html/select (fetch-url *base-url*) [:td.subtext :> html/first-child])))
+
+(defn print-headlines-and-points []
+  (doseq [line (map #(str %1 " (" %2 ")") (hn-headlines) (hn-points))]
+    (println line)))
